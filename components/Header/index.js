8af2bd5eaa2112794fcdaa3f95d4b445076d91ea
damico williams <damicowilliams@gmail.com>
2019-09-14 18:27:13 -0700

    header done

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,31 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    //create elements
+    const wrapper = document.createElement('div');
+    const date = document.createElement('span');
+    const head = document.createElement('h1');
+    const degree = document.createElement('span');
+    
+    //create structure
+    wrapper.appendChild(span);
+    wrapper.appendChild(head);
+    wrapper.appendChild(degree);
+
+    //add content
+    date.textContent = 'SMARCH 28, 2019';
+    head.textContent = 'Lambda Times';
+    degree.textContent = '98°';
+
+    //add styles
+    wrapper.classList.add('header');
+    date.classList.add('date');
+    head.classList.add('h1');
+    degree.classList.add('temp');
+
+    return wrapper;
+
+}
+
+const add = document.querySelector('.header-container')
